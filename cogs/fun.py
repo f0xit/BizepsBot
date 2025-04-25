e362d36833f697d87c10914f2560c52d61110b61
f0xit <m.loe@gmx.de>
2025-04-26 01:07:36 +0200

    serious bugfixing

--- cogs/fun.py
+++ cogs/fun.py
@@ -9,15 +9,6 @@ from discord.ext import commands
 from Main import _get_banned_users, _is_banned, logging
 
 
-# Checks
-def _is_zuggi(ctx):
-    return ctx.author.id == 232561052573892608
-
-
-def _is_nouwuchannel(ctx):
-    return ctx.channel.category_id != 539547423782207488 and ctx.channel.id != 539549544585756693
-
-
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -63,14 +54,12 @@ class Fun(commands.Cog):
 
     # Commands
     @commands.slash_command(name="josch", description="Entwickler...", brief="Entwickler...")
-    @commands.cooldown(1, 10, commands.BucketType.user)
     async def _blamedevs(self, ctx):
         await ctx.defer()
         await ctx.followup.send(file=discord.File("memes/josch700 (josch700)/josch.png"))
         logging.info(f"{ctx.author} blamed the devs.")
 
     @commands.slash_command(name="turnup", description="Was für Saft?", brief="Was für Saft?")
-    @commands.cooldown(1, 10, commands.BucketType.user)
     async def _orangejuice(self, ctx):
         if str(ctx.author) != "Schnenko#9944":
             await ctx.respond("Frag nicht was für Saft, einfach Orangensaft! Tuuuuuuuurn up! Fassen Sie mich nicht an!")
@@ -80,11 +69,10 @@ class Fun(commands.Cog):
 
     @commands.slash_command(name="ehrenmann", description="Der erwähnte User ist ein Ehrenmann!", brief="Der erwähnte User ist ein Ehrenmann!")
     async def _ehrenmann(self, ctx, user: discord.Option(discord.User, description="Wähle den ehrenhaften User", required=True)):
-        await ctx.respond(f"{user.mention}, du bist ein gottverdammter Ehrenmann!<:Ehrenmann:955905863154036748>")
+        await ctx.respond(f"{user.mention}, du bist ein gottverdammter Ehrenmann! <:djmaggus:1095706395355127808>")
         logging.info(f"{ctx.author} wanted to let {user.name} know he is an ehrenmann.")
 
     @commands.slash_command(name="lebonk", description="Don't mess with him...", brief="Don't mess with him...")
-    @commands.cooldown(1, 30, commands.BucketType.user)
     async def _lebonk(self, ctx):
         LastMessages = await ctx.channel.history(limit=1).flatten()
         LastMessage = LastMessages[0]
@@ -125,7 +113,6 @@ class Fun(commands.Cog):
         await ctx.respond(f"Elisabot sagt nein, {random.SystemRandom().choice(ElisabotList)}.")
 
     @commands.slash_command(name="nein", description="Nein.")
-    @commands.check(_is_zuggi)
     async def _zuggisaysno(self, ctx: discord.ApplicationContext):
         LastMessages = await ctx.channel.history(limit=1).flatten()
         LastMessage = LastMessages[0]
@@ -137,8 +124,6 @@ class Fun(commands.Cog):
             await ctx.respond("Nachricht wurde verneint!", ephemeral=True)
 
     @commands.slash_command(name="uwu", description="Weebt die Message UwU")
-    @commands.cooldown(1, 60, commands.BucketType.user)
-    @commands.check(_is_nouwuchannel)
     async def _uwuthis(self, ctx):
         LastMessages = await ctx.channel.history(limit=1).flatten()
         if LastMessages[0].author == self.bot.user:
@@ -152,12 +137,10 @@ class Fun(commands.Cog):
         logging.info(f"{ctx.author} hat die Nachricht [{LastMessages[0].content}] geUwUt.")
 
     @commands.slash_command(name="pr", description="Pullrequests einreichen zur Übername... oder so")
-    @commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
     async def _dont_ask(self, ctx):
         await ctx.respond("https://i.redd.it/before-t8-was-announced-harada-said-dont-ask-me-for-shit-v0-e4arzhnywrda1.jpg?width=451&format=pjpg&auto=webp&s=0ec112c803a3a927add3aad4eabafcb83a0bedec")
 
     @commands.slash_command(name="schnabi", description="Er malt gerne!", brief="Er malt gerne!")
-    @commands.cooldown(1, 10, commands.BucketType.user)
     async def _schnabi(self, ctx: discord.context.ApplicationContext):
         if (waifu_data := await self.get_waifu_img()) is None:
             await ctx.respond("Heute keine Waifus für dich, fass mal Gras an :)")
@@ -174,43 +157,13 @@ class Fun(commands.Cog):
 
     @commands.Cog.listener("on_message")
     async def _uwumsg(self, message):
-        if isinstance(message.channel, discord.channel.DMChannel) or message.channel.category_id == 539547423782207488 or message.channel.id == 539549544585756693 or message.author == self.bot.user:
+        if isinstance(message.channel, discord.channel.DMChannel):
             return
         if random.randint(0, 75) == 1 and len(message.content) > 50 and "http://" not in message.content and "https://" not in message.content:  # noqa: S311
             LastMessageContent = message.content
             flags = uwuify.SMILEY | uwuify.YU
             await message.channel.send(f"{uwuify.uwu(LastMessageContent, flags=flags)} <:UwU:870283726704242698>")
             logging.info(f"The message [{LastMessageContent}] was UwUed.")
-
-    @_lebonk.error
-    async def _bonk_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            await ctx.respond(f"Dieser Befehl ist noch im Cooldown. Versuche in {int(error.retry_after)} Sekunden nochmal jemand zu bonken.", ephemeral=True)
-            logging.warning(f"{ctx.author} wanted to spam the Bonkcommand!")
-
-    @_zuggisaysno.error
-    async def _zuggisaysno_error(self, ctx, error):
-        if isinstance(error, discord.errors.CheckFailure):
-            await ctx.respond("Du bist nicht Zuggi.", ephemeral=True)
-            logging.warning(f"{ctx.author} wanted to nein something, but is not Zuggi.")
-
-    @_uwuthis.error
-    async def _uwuthis_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            await ctx.respond(f"Dieser Befehl ist noch im Cooldown. Versuch es in {int(error.retry_after)} Sekunden nochmal.", ephemeral=True)
-            logging.warning(f"{ctx.author} wanted to spam the UwUcommand!")
-
-    @_dont_ask.error
-    async def _dont_ask_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            await ctx.respond(f"Dieser Befehl ist noch im Cooldown. Versuch es in {int(error.retry_after)} Sekunden nochmal.", ephemeral=True)
-            logging.warning(f"{ctx.author} wanted to spam the Haradacommand!")
-
-    @_schnabi.error
-    async def _schnabi_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            await ctx.respond(f"Dieser Befehl ist noch im Cooldown. Versuch es in {int(error.retry_after)} Sekunden nochmal.", ephemeral=True)
-            logging.warning(f"{ctx.author} wanted to spam the Schnabicommand!")
 
 
 def setup(bot):
